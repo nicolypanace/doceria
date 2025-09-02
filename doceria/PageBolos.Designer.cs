f4f5e8110096294eebd7cbd6e5176459b961e133
nicolypanace <n.panace@aluno.ifsp.edu.br>
2025-09-02 12:44:02 -0300

    a

--- doceria/PageBolos.Designer.cs
+++ doceria/PageBolos.Designer.cs
@@ -46,9 +46,11 @@
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(239, 102);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(39, 16);
             this.label7.TabIndex = 19;
             this.label7.Text = "label7";
             this.label7.Click += new System.EventHandler(this.label7_Click);
@@ -103,6 +105,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
             this.label6.Location = new System.Drawing.Point(372, 102);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(35, 13);
